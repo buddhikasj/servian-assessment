a1a941611a7239534073f621fb70a75ed0deba83
Buddhika Jayawardena <ext.buddhika.j@grabtaxi.com>
2022-08-24 09:51:38 +0800

    added provider default tagging

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 terraform {
   # NOTE : Shared remote backend such as consul can be used to maintain the statefile if multiple administrators are managing the infra via terraform
-  # since this is an one off setup we will use local state files 
+  # since this is an one off setup i will use local state files 
   # backend "consul" {
   #   address = "consul.example.com"
   #   scheme  = "https"
   # ## consult access token can be provided via envionment variables or partial configuration
   #   path    = "full/path"
   # }
-  # you can also use worspaces to create replicas of the environment for prod, dev and staging, It is not used here me keep things simple
+  # you can also use worspaces to create replicas of the environment for prod, dev and staging, It is not used here t keep things simple
 
   required_providers {
     aws = {
